102f90eaea9a65d888cb8eab1292c03d46815928
simalshakha <sshakha350@gmail.com>
2025-08-12 18:34:37 +0545

    final

--- rectangle_numbering.py
+++ rectangle_numbering.py
@@ -3,7 +3,9 @@ import matplotlib.pyplot as plt
 from math import hypot
 from itertools import combinations
 import os
-#this is to calculate distance between points
+import numpy as np
+
+# this is to calculate distance between points
 def max_distance(points):
     pts = points.reshape(-1, 2)
     max_d = 0
@@ -13,17 +15,48 @@ def max_distance(points):
             max_d = dist
     return max_d
 
-#find all rectangles
+# Calculate angle between vectors pt0->pt1 and pt0->pt2
+def angle(pt1, pt2, pt0):
+    dx1 = pt1[0] - pt0[0]
+    dy1 = pt1[1] - pt0[1]
+    dx2 = pt2[0] - pt0[0]
+    dy2 = pt2[1] - pt0[1]
+    # dot product and magnitude
+    dot = dx1 * dx2 + dy1 * dy2
+    mag1 = np.sqrt(dx1*dx1 + dy1*dy1)
+    mag2 = np.sqrt(dx2*dx2 + dy2*dy2)
+    if mag1 * mag2 == 0:
+        return 0
+    cos_angle = dot / (mag1 * mag2)
+    # Clamp cos_angle to [-1, 1] to avoid numerical errors
+    cos_angle = max(min(cos_angle, 1), -1)
+    ang = np.arccos(cos_angle) * 180 / np.pi
+    return ang
+
+# Check if approx polygon is rectangle by verifying all angles ~90 degrees
+def is_rectangle(approx, max_deviation=10):
+    if len(approx) != 4:
+        return False
+    for i in range(4):
+        pt0 = approx[i][0]
+        pt1 = approx[(i + 1) % 4][0]
+        pt2 = approx[(i + 3) % 4][0]
+        ang = angle(pt1, pt2, pt0)
+        if abs(ang - 90) > max_deviation:
+            return False
+    return True
+
+# find only rectangles
 def find_rectangles(contours):
     rects_found = []
     for i, cnt in enumerate(contours):
         approx = cv2.approxPolyDP(cnt, 0.02 * cv2.arcLength(cnt, True), True)
-        if len(approx) == 4:
+        if len(approx) == 4 and is_rectangle(approx):
             x, y, w, h = cv2.boundingRect(cnt)
             rects_found.append((x, y, w, h, i))
     return rects_found
 
-#find lines which are inside the rectangle and sort them based on their length
+# find lines which are inside the rectangle and sort them based on their length
 def find_and_number_lines_inside(contours, hierarchy, rects):
     lines_inside = []
     for i, cnt in enumerate(contours):
@@ -41,36 +74,29 @@ def find_and_number_lines_inside(contours, hierarchy, rects):
     line_nums = {idx: n+1 for n, (_, idx) in enumerate(lines_inside)}
     return lines_inside, line_nums
 
-#drawing and assigning numbers to line according to their length
+# drawing and assigning numbers to line according to their length
 def draw_lines_and_numbers(img, contours, lines_inside, line_nums):
     for dist, idx in lines_inside:
         cv2.drawContours(img, [contours[idx]], -1, (255, 0, 0), 2)
         pt = tuple(contours[idx][0][0])  
         cv2.putText(img, str(line_nums[idx]), pt, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
 
-
-
-#put the path of ur input image here
+# put the path of your input image here
 img = cv2.imread('image.png')
-
-
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 _, thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY_INV)
 contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 output = img.copy()
 
-
 rects = find_rectangles(contours)
 lines_inside, line_nums = find_and_number_lines_inside(contours, hierarchy, rects)
 draw_lines_and_numbers(output, contours, lines_inside, line_nums)
 
-
 plt.imshow(cv2.cvtColor(output, cv2.COLOR_BGR2RGB))
+plt.axis('off')
+plt.show()
 
 output_dir = 'task1-output'
 os.makedirs(output_dir, exist_ok=True)
 cv2.imwrite(os.path.join(output_dir, 'output_image.png'), output)
-
-plt.axis('off')
-plt.show()
